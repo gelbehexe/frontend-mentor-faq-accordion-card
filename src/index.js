72137b1883acacaf7112379b9999be5be5c64916
Deborah White <deborah.white1503@icloud.com>
2022-03-17 21:48:03 +0100

    Workaround for hot reload bug

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./css/styles.scss"
+
+// noinspection JSUnresolvedVariable
+if (module.hot) {
+    // workaround for hot reload bug: @see https://github.com/jantimon/html-webpack-plugin/issues/1709
+    require("./assets/images/favicon-32x32.png")
+}
